3efe85f9f0232eb3e51ab8f00f03b2ff71418c96
Yoshiko Hatanaka <yohatana@c6r3s6.42tokyo.jp>
2025-04-11 20:07:35 +0900

    start

---
+++ srcs/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 19:56:46 by yohatana          #+#    #+#             */
+/*   Updated: 2025/04/11 20:06:39 by yohatana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+/*
+	memset, printf, malloc, free, write,
+usleep, gettimeofday, pthread_create,
+pthread_detach, pthread_join, pthread_mutex_init,
+pthread_mutex_destroy, pthread_mutex_lock,
+pthread_mutex_unlock
+*/
+
+// 使用可能関数を使ってみる
+int	main(int argc, char **argv)
+{
+	return (0);
+}
